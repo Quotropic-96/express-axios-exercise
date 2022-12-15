ea6d09e229c86838808fca3faea7a8d50faede5b
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-15 21:10:27 +0100

    Exercise complete

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 const express = require('express');
+const axios = require('axios');
+const { response } = require('express');
 
 const app = express();
 
 // Iteration 1
 app.get('/pikachu', (req, res, next) => {
-  // ...
-  // https://pokeapi.co/api/v2/pokemon/pikachu
+  axios.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+    .then(response => {
+      res.status(200).json(response.data);
+    })
+    .catch(err => console.log(err));
 })
 
 // Iteration 2
-// app.get(...)
-// https://api.spacexdata.com/v4/rockets
-
+app.get('/rockets', async (req, res, next) => {
+  try {
+    const response = await axios.get('https://api.spacexdata.com/v4/rockets');
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
+})
 
 // Iteration 3
-// app.get(...)
-// https://randomuser.me/api/
-
+app.get('/randomuser', async (req, res, next) => {
+  try {
+    const response = await axios.get('https://randomuser.me/api/');
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
+})
 
 // Iteration 4
 app.get('/', (req, res) => {
-  res.send('<h4>All working here ✅</h4>')
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 app.listen(3000, () => { console.log('Server running on port 3000 🛫') })
